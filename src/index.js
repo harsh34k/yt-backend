9f6d8e7d4c4e6c03cab3ab097aca8142fd3f5a95
Harsh Kumar <harsh34k@gmail.com>
2024-01-19 18:19:17 +0530

    updated uitls-apiError,apiResponse,asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// require("dotenv").config({ path: "./env" })
+
 import dotenv from "dotenv"
 import connnectDB from "./db/index.js"
 
@@ -6,4 +6,12 @@ dotenv.config({
     path: "./.env"
 })
 
-connnectDB();
+connnectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
